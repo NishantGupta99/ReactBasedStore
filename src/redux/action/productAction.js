dde59050777327e022ee597deade0f30c778bebd
nishant Gupta <ngactive99@gmail.com>
2021-06-06 15:53:55 +0530

    starting to make the store

---
+++ src/redux/action/productAction.js
@@ -0,0 +1,15 @@
+import { ActionTypes } from "../contents/action-types";
+export const setProducts = (products) => {
+    return {
+        type: Actiontypes.SET_PRODUCTS,
+        payload: products,
+    };
+};
+
+export const selectedProducts = (products) => {
+    return {
+        type: Actiontypes.SELECTED_PRODUCTS,
+        payload: products,
+    };
+};
+
